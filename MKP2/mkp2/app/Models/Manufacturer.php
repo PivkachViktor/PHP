a5f182b22429ad7781da012a3aff45fa524f1658
Viktor <Pivkach.viktor@student.uzhnu.edu.ua>
2023-12-13 10:45:33 +0200

    First task

---
+++ MKP2/mkp2/app/Models/Manufacturer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Manufacturer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['brand','country','phone','email','created_at','updated_at'];
+}
